c350a306ea45d761e1b13ff85f90b0d8ae55895c
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-07-30 16:32:36 -0600

    Add basic stylings and bones of page. Makes a request to the YouTube API and returns the title of songs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,41 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).on('turbolinks:load', function(){
+var key = 'AIzaSyBxDR9n9N0Hg1C2-7_5F1hDGjs4gu61bso'
+
+$(document).ready(function() {
+	var track = {
+		protec: [
+			'https://www.youtube.com/watch?v=OCD99jMMuh0',
+			'https://www.youtube.com/watch?v=AXT4dxCrmEI',
+			'https://www.youtube.com/watch?v=_12QwdU1e6Q'
+		],
+		attac: [
+			'https://www.youtube.com/watch?v=h8rsGxmlBRU',
+			'https://www.youtube.com/watch?v=cAn4lQyTJfI',
+			'https://www.youtube.com/watch?v=GTG7TPlvpMI'
+		]
+	};
+
+	var youtubeVid = new RegExp('(?:youtube\.com\/watch\\?v=|youtu\.?be\/)([^ ]+)');
+
+	$.each(track, function(type, list) {
+		$('#bar').append('<div id="' + type + '"></div>');
+
+		$.each(list, function(i, song) {
+			try {
+				var id = youtubeVid.exec(song)[1];
+
+				$.getJSON('https://www.googleapis.com/youtube/v3/videos?id=' + id + '&key=' + key + '&part=snippet,contentDetails', function(data) {
+					var title = data.items[0].snippet.title;
+
+					$('#' + type).append('<a><li>' + title + '</li></a>');
+				});
+			}
+
+			catch(err) {
+				alert(err)
+			}
+		});
+	});
 });
